d51e3532fe92aa601007489cc22a0b2bf44aa04d
pratiklimbachiya <pratik.limbachiya@minddeft.com>
2021-12-28 12:23:57 +0530

    Url update

--- config/database.js
+++ config/database.js
@@ -6,7 +6,8 @@ const mongoUrl = `mongodb+srv://minddeft-user:9e*p&uct@cluster-staging.mob9k.mon
 
 mongoose
   .connect(
-    `mongodb://${process.env.DATABASEURL}:${process.env.DATABSEPORT}/${process.env.DATABASE}`,
+    // `mongodb://${process.env.DATABASEURL}:${process.env.DATABSEPORT}/${process.env.DATABASE}`,
+    mongoUrl,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
